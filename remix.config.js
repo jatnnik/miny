4aa9e68c81be1613a6fd604f10608f9bd80a1f4a
Jannik Baranczyk <jannik.baranczyk@gmail.com>
2022-04-02 22:11:15 +0200

    fix remix.config.js for vercel

--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,11 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
-  serverBuildDirectory: 'build',
-  devServerPort: 8002,
+  // appDirectory: 'app',
+  // assetsBuildDirectory: 'public/build',
+  // publicPath: '/build/',
+  // serverBuildDirectory: 'build',
+  // devServerPort: 8002,
   ignoredRouteFiles: ['.*'],
   server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   serverBuildTarget: 'vercel',
