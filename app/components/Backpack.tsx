a9563f8f636e7cb5557f7739c3e4467a16f12b75
Jannik Baranczyk <jannik.baranczyk@gmail.com>
2022-09-20 23:50:00 +0200

    add reusable <Backpack /> component

---
+++ app/components/Backpack.tsx
@@ -0,0 +1,13 @@
+export default function Backpack({ size = 32 }) {
+  return (
+    <div className="block rounded-lg bg-red-400 bg-opacity-20 p-2">
+      <img
+        src="/backpack.png"
+        className="h-8"
+        alt="Rucksack Emoji"
+        height={size}
+        width={size}
+      />
+    </div>
+  )
+}
