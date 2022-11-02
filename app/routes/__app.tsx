0defd09bdd254e364d55a09cfd929ac1870594e9
Jannik Baranczyk <jannik.baranczyk@gmail.com>
2022-11-02 14:37:45 +0100

    render menu only when user is logged in

--- app/routes/__app.tsx
+++ app/routes/__app.tsx
@@ -1,10 +1,20 @@
-import { Link, Outlet } from "@remix-run/react"
+import type { LoaderArgs } from "@remix-run/node"
+import { json } from "@remix-run/node"
+import { Link, Outlet, useLoaderData } from "@remix-run/react"
 
 import Container from "~/components/shared/Container"
 import Footer from "~/components/shared/Footer"
 import Menu from "~/components/shared/Menu"
+import { getUser } from "~/session.server"
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const user = await getUser(request)
+  return json({ isLoggedIn: !!user })
+}
 
 export default function AppRoute() {
+  const { isLoggedIn } = useLoaderData<typeof loader>()
+
   return (
     <Container>
       <div className="h-6 sm:h-8"></div>
@@ -18,7 +28,16 @@ export default function AppRoute() {
             <h1>miny</h1>
           </Link>
         </div>
-        <Menu />
+        {isLoggedIn ? (
+          <Menu />
+        ) : (
+          <Link
+            to="login"
+            className="rounded-md bg-slate-300 px-4 py-2 text-sm font-medium transition-colors hover:bg-slate-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-slate-700 focus-visible:ring-opacity-75"
+          >
+            Anmelden
+          </Link>
+        )}
       </div>
       <div className="h-6"></div>
       <Outlet />
